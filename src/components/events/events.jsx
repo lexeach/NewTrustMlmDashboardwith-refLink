033018daaa9efd564d55901f7cb1aa970b9f4b5e
lexeach <80155340+lexeach@users.noreply.github.com>
2022-12-18 15:18:34 +0530

    Update events.jsx

--- src/components/events/events.jsx
+++ src/components/events/events.jsx
@@ -189,7 +189,7 @@ const EventsList = () => {
         <div className="col-lg-6 col-md-12 col-sm-12 grid-margin">
           <div className="card">
             <div className="card-body">
-              <h5>User List </h5>
+              <h5>Check Wallet by ID </h5>
               <div className="row">
                 <div className="col-12 col-sm-12 col-xl-12 my-auto">
                   <div className="d-flex d-sm-block d-md-flex align-items-center">
@@ -260,7 +260,7 @@ const EventsList = () => {
         <div className="col-lg-12 col-md-12 col-sm-12 grid-margin">
           <div className="card">
             <div className="card-body">
-              <h5>Users</h5>
+              <h5>User Details by wallet address</h5>
               <div className="row">
                 <div className="col-12 col-sm-12 col-xl-12 my-auto">
                   <div className="d-flex d-sm-block d-md-flex align-items-center">
@@ -295,37 +295,31 @@ const EventsList = () => {
                       <div className="d-flex">
                         <h4 className="heading_"> Auto Pool Pay Reciver :- </h4>
                         <h4> {users_autopoolPayReciever}</h4>
+                     
                       </div>
                       <div className="d-flex">
-                        <h4 className="heading_"> Batch Paid :- </h4>
-                        <h4> {users_batchPaid}</h4>
-                      </div>
-                      <div className="d-flex">
-                        <h4 className="heading_"> ID :- </h4>{" "}
+                        <h4 className="heading_">My User ID :- </h4>{" "}
                         <h4> {users_id}</h4>
                       </div>
                       <div className="d-flex">
-                        <h4 className="heading_"> Income :- </h4>
+                        <h4 className="heading_"> Total Income :- </h4>
                         <h4> {users_income}</h4>
                       </div>
+                      
                       <div className="d-flex">
-                        <h4 className="heading_"> is Exist :- </h4>
-                        <h4> {users_isExist}</h4>
-                      </div>
-                      <div className="d-flex">
-                        <h4 className="heading_"> Level Income Recived :- </h4>
+                        <h4 className="heading_"> Total Level Income :- </h4>
                         <h4> {users_levelIncomeReceived}</h4>
                       </div>
                       <div className="d-flex">
-                        <h4 className="heading_"> Missed Pool Payment :- </h4>
+                        <h4 className="heading_"> Missed Autopool Income :- </h4>
                         <h4> {users_missedPoolPayment}</h4>
                       </div>
                       <div className="d-flex">
-                        <h4 className="heading_"> Reffered User :- </h4>
+                        <h4 className="heading_"> Total Direct :- </h4>
                         <h4> {users_referredUsers}</h4>
                       </div>
                       <div className="d-flex">
-                        <h4 className="heading_"> Refferrer ID :- </h4>
+                        <h4 className="heading_"> My Sposor :- </h4>
                         <h4> {users_referrerID}</h4>
                       </div>
                     </div>
